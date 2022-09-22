5a8b0409f8a1c37a10bdad76b045875c684ad0ab
Shiran Dudy <shirdu2@gmail.com>
2022-09-21 18:59:09 -0600

    more code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 import torchaudio
 from speechbrain.pretrained import EncoderClassifier, VAD
 import re
@@ -7,6 +8,7 @@ import pdb
 
 classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
 
+cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
 
 wav_folder = "../wav/"
@@ -21,11 +23,6 @@ for wav_file in os.listdir(wav_folder):
 
     # check if current path is a file
     if file_exists(txt):
-
-        # apply x-vector for diarization
-        signal, fs =torchaudio.load(wav)
-        embeddings = classifier.encode_batch(signal)
-
         # apply VAD
         VAD = VAD.from_hparams(source="speechbrain/vad-crdnn-libriparty", savedir="pretrained_models/vad-crdnn-libriparty")
         # 1- Let's compute frame-level posteriors first
@@ -49,4 +46,22 @@ for wav_file in os.listdir(wav_folder):
         # 7- Double-check speech segments (optional).
         boundaries = VAD.double_check_speech_segments(boundaries, wav,  speech_th=0.5)
 
-     
+        # get the sample rate
+        signal, fs = torchaudio.load(wav)
+        for (begin1, end1), (begin2, end2)  in zip(boundaries, boundaries[1:]):
+            # apply x-vector for diarization
+            pdb.set_trace()
+            # below a fixed threshold
+            if begin2 - end1 < threshold:
+                embeddings1 = classifier.encode_batch(signal[0][round(float(begin1))*fs: round(float(end1))*fs])
+                embeddings2 = classifier.encode_batch(signal[0][round(float(begin2))*fs: round(float(end2))*fs])
+                 # measure distance from the dictionary
+                 sp1 = []
+                 sp2 = []
+                 for speaker in speakers:
+                     sp1.append(cos(speaker, embeddings1))               
+                     sp2.append(cos(speaker, embeddings2))
+                 # if are not the same speaker
+                 if sp1.index(min(sp1)) != sp2.index(min(sp2)):
+                     print("{D[argmin(sp2)]} interrupted to {D[argmin(sp1)]} at {begin} second")
+
