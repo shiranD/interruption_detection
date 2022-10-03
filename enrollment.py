61dc2d86d23e072f3cf6da49a5902497dfaaab9b
Shiran Dudy <shirdu2@gmail.com>
2022-10-03 16:31:36 -0600

    gdrive enrollment

---
+++ enrollment.py
@@ -0,0 +1,37 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from speechbrain.pretrained import EncoderClassifier
+import torchaudio
+import os
+
+def enroll_speakers(names):
+    # input speakers
+    # output dict of speakers
+    
+    gauth = GoogleAuth()
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("mycreds.txt")
+    #gauth.LocalWebserverAuth()
+    # Creates local webserver and auto handles authentication
+    drive = GoogleDrive(gauth)
+    classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
+    speakers_dict = {}
+    #file0 = "enrollment_student-006.wav"
+    for file0 in names:
+        local_download_path="/Users/shdu9019/Documents/interruption_detection/interruption_detection/g_files"
+        path = "https://drive.google.com/drive/u/1/folders/1vqIVEKdWVvZf3KBVgdOJ8_KbuzM_dndK/"
+        # Auto-iterate through all files that matches this query
+        file_list = drive.ListFile({'q': "'1vqIVEKdWVvZf3KBVgdOJ8_KbuzM_dndK' in parents"}).GetList()
+        for file1 in file_list:
+          print('title: %s, id: %s' % (file1['title'], file1['id']))
+          if file1['title'] == file0:
+              fname = os.path.join(local_download_path, file1['title'])
+              print('downloading to {}'.format(fname))
+              f_ = drive.CreateFile({'id': file1['id']})
+              f_.GetContentFile(fname)
+              signal, fs = torchaudio.load(fname)
+              embeddings = classifier.encode_batch(signal)
+              speakers_dict[file0]=embeddings
+              print("is", fs)
+              break
+    return speakers_dict
--- main.py
+++ main.py
@@ -1,53 +1,51 @@
-import torch
+from torch import nn
 import torchaudio
-from speechbrain.pretrained import EncoderClassifier, VAD
+from speechbrain.pretrained import EncoderClassifier
 import re
 import os
+import sys
 from os.path import exists as file_exists
 import pdb
+from vad_class import VADer
+from enrollment import enroll_speakers
 
 classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", savedir="pretrained_models/spkrec-xvect-voxceleb")
 
 cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
-
 wav_folder = "../wav/"
 transcripts_folder = "../transcripts/"
+vad = VADer()
+
 for wav_file in os.listdir(wav_folder):
     print(wav_file)
 
     # replace .wav with .txt and find its transcript in transcripts folder
     txt_file = re.sub(".wav", ".txt", wav_file)
     txt = os.path.join(transcripts_folder, txt_file)
-    wav = os.path.join(wav_folder, wav_file)
 
     # check if current path is a file
     if file_exists(txt):
-        # apply VAD
-        VAD = VAD.from_hparams(source="speechbrain/vad-crdnn-libriparty", savedir="pretrained_models/vad-crdnn-libriparty")
-        # 1- Let's compute frame-level posteriors first
-        prob_chunks = VAD.get_speech_prob_file(wav)
-
-        # 2- Let's apply a threshold on top of the posteriors
-        prob_th = VAD.apply_threshold(prob_chunks).float()
-
-        # 3- Let's now derive the candidate speech segments
-        boundaries = VAD.get_boundaries(prob_th)
-        
-        # 4- Apply energy VAD within each candidate speech segment (optional)
-        boundaries = VAD.energy_VAD(wav,boundaries)
-        
-        # 5- Merge segments that are too close
-        boundaries = VAD.merge_close_segments(boundaries, close_th=0.250)
-        
-        # 6- Remove segments that are too short
-        boundaries = VAD.remove_short_segments(boundaries, len_th=0.250)
-        
-        # 7- Double-check speech segments (optional).
-        boundaries = VAD.double_check_speech_segments(boundaries, wav,  speech_th=0.5)
-
+        # enroll participants
+        names = []
+        with open(txt, "r") as f:
+            for line in f.readlines():
+               name = line.split('\t')[0].lower()
+               if 'other' not in name and 'teacher' not in name:
+                   name = name.replace(' ', '-')
+                   name = 'enrollment_'+name+'.wav'
+                   names.append(name)
+            names = set(names)
+            enroll_dict = enroll_speakers(names)
+        print(enroll_dict)
+        sys.exit()
+        # prep wav path 
+        wav = os.path.join(wav_folder, wav_file)
+        # apply vad
+        boundaries = vad.chunk(wav)
         # get the sample rate
         signal, fs = torchaudio.load(wav)
+        
         for (begin1, end1), (begin2, end2)  in zip(boundaries, boundaries[1:]):
             # apply x-vector for diarization
             pdb.set_trace()
@@ -55,13 +53,13 @@ for wav_file in os.listdir(wav_folder):
             if begin2 - end1 < threshold:
                 embeddings1 = classifier.encode_batch(signal[0][round(float(begin1))*fs: round(float(end1))*fs])
                 embeddings2 = classifier.encode_batch(signal[0][round(float(begin2))*fs: round(float(end2))*fs])
-                 # measure distance from the dictionary
-                 sp1 = []
-                 sp2 = []
-                 for speaker in speakers:
-                     sp1.append(cos(speaker, embeddings1))               
-                     sp2.append(cos(speaker, embeddings2))
-                 # if are not the same speaker
-                 if sp1.index(min(sp1)) != sp2.index(min(sp2)):
-                     print("{D[argmin(sp2)]} interrupted to {D[argmin(sp1)]} at {begin} second")
+                # measure distance from the dictionary
+                sp1 = []
+                sp2 = []
+                for speaker in speakers:
+                    sp1.append(cos(speaker, embeddings1))               
+                    sp2.append(cos(speaker, embeddings2))
+                # if are not the same speaker
+                if sp1.index(min(sp1)) != sp2.index(min(sp2)):
+                    print("{D[argmin(sp2)]} interrupted to {D[argmin(sp1)]} at {begin} second")
 
